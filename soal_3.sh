9c2207205b0f6e63bd8928c12b4c533bd065e574
nadfk <naaddiiaakusuma@gmail.com>
2025-10-01 22:41:37 +0700

    Create soal_3.sh

---
+++ soal_3.sh
@@ -0,0 +1,9 @@
+#config melkor
+up echo nameserver 192.168.122.1 > /etc/resolv.conf
+
+#config manwe
+up echo nameserver 192.168.122.1 > /etc/resolv.conf
+#config varda
+up echo nameserver 192.168.122.1 > /etc/resolv.conf
+#config ulmo
+up echo nameserver 192.168.122.1 > /etc/resolv.conf
