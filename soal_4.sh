339fd668f00e3a56919c739c2af0416fcfbbd81f
nadfk <naaddiiaakusuma@gmail.com>
2025-10-01 23:55:08 +0700

    Delete soal_4.sh

--- soal_4.sh
+++
@@ -1,9 +0,0 @@
-#config melkor
-up echo nameserver 192.168.122.1 > /etc/resolv.conf
-
-#config manwe
-up echo nameserver 192.168.122.1 > /etc/resolv.conf
-#config varda
-up echo nameserver 192.168.122.1 > /etc/resolv.conf
-#config ulmo
-up echo nameserver 192.168.122.1 > /etc/resolv.conf
